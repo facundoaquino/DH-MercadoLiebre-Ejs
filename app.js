2d8a524e64b9ddc20371eae1048329803cf40984
Facundo Aquino <facun90@live.com>
2020-11-16 22:01:27 -0300

    cambios y estilos

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const app = express()
- 
+const bodyParser =require('body-parser') 
+
+
 app.use(express.static('public'));
 app.use(express.static('views'));
+
+/*---------------------- probando express para llenar el body request ---------------------*/
+
+// app.use(express.json())
+// app.use(express.urlencoded({extended:true}))
+
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended:true}))
+
 
 app.get('/', (req,res)=>{
      
     res.sendFile( __dirname + '/views/index.html')
 
 })
+
+// app.post('/registrer',(req,res)=>{
+//     console.log(req.body)
+//     res.sendFile( __dirname + '/views/index.html')
+// })
 
 // app.get('*', function (req, res) {
 //     res.sendFile(__dirname + '/public/' + req.url)
@@ -19,6 +35,17 @@ app.get('/newsletter', (req,res)=>{
     res.sendFile( __dirname + '/views/newsletter.html')
 
 })
+app.get('/registrer', (req,res)=>{
+     
+    res.sendFile( __dirname + '/views/registrer.html')
 
+})
 
+app.get('/login', (req,res)=>{
+     
+    res.sendFile( __dirname + '/views/login.html')
+
+})
+
+  
 app.listen(3050)
