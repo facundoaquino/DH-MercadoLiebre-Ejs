087d6bf6347db4b44b418a8bb3fca3551d3a72a8
Facundo Aquino <facun90@live.com>
2020-12-21 21:49:48 -0300

    changes controllers

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const path = require('path')
+const { disconnect } = require('process')
+const setID = require('../helpers/setID')
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json')
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'))
@@ -7,6 +9,9 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'))
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
 
 products.forEach((product) => (product.price = toThousand(product.price)))
+
+/*---------------------- multer ---------------------*/
+
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
@@ -14,7 +19,7 @@ const controller = {
 	},
 
 	// Detail - Detail from one product
-	detail: (req, res) => {
+	detail: (req, res, next) => {
 		const product = products.find((pro) => pro.id == req.params.id)
 
 		if (product.discount) {
@@ -26,30 +31,62 @@ const controller = {
 	},
 
 	// Create - Form to create
-	create: (req, res) => {
+	create: (req, res, next) => {
 		res.render('product-create-form')
 	},
 
 	// Create -  Method to store
-	store: (req, res) => {
-		res.send(req.body)
+	store: (req, res, next) => {
+		const ids = products.map((product) => product.id)
+		const idGenerate = setID(ids)
+		const product = {
+			...req.body,
+			id: idGenerate,
+			image: req.files[0].filename,
+		}
+		product.discount = Number(product.discount)
+		products.push(product)
+		const productJson = JSON.stringify(products)
+		fs.writeFileSync(productsFilePath, productJson)
+
+		res.redirect('/')
 	},
 
 	// Update - Form to edit
 
-	edit: (req, res) => {
+	edit: (req, res, next) => {
 		const product = products.find((product) => product.id == req.params.id)
 		res.render('product-edit-form', { product, title: product.name })
 	},
 	// Update - Method to update
-	update: (req, res) => {
-		res.send({ id: req.params.id, ...req.body })
+	update: (req, res, next) => {
+		const { name, price, discount, description, category } = req.body
+		products.forEach((product) => {
+			if (product.id == req.params.id) {
+				// product = { ...product, ...req.body, image: req.files[0].filename }
+				product.name = name
+				product.price = price
+				product.discount = Number(product.discount)
+				product.image = req.files[0].filename
+				product.category = category
+				product.description = description
+			}
+		})
+
+		const productJson = JSON.stringify(products)
+		fs.writeFileSync(productsFilePath, productJson)
+
+		res.redirect('/')
 	},
 
 	// Delete - Delete one product from DB
 	destroy: (req, res) => {
-		const product = products.find((product) => product.id == req.params.id)
-		res.send({ deleted: true, ...product })
+		const productsNotDeleted = products.filter((product) => product.id != req.params.id)
+
+		const productJson = JSON.stringify(productsNotDeleted)
+		fs.writeFileSync(productsFilePath, productJson)
+
+		res.redirect('/')
 	},
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,19 +5,34 @@ const router = express.Router()
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController')
 
+/*---------------------- multer/path ---------------------*/
+const path = require('path')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'public/images/products')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+	},
+})
+
+const upload = multer({ storage: storage })
+
 /*** GET ALL PRODUCTS ***/
 router.get('/', productsController.index)
 
 // /*** CREATE ONE PRODUCT ***/
 router.get('/create', productsController.create)
-router.post('/create', productsController.store)
+router.post('/create', upload.any(), productsController.store)
 
 // /*** GET ONE PRODUCT ***/
 router.get('/detail/:id', productsController.detail)
 
 // /*** EDIT ONE PRODUCT ***/
 router.get('/edit/:id', productsController.edit)
-router.put('/:id', productsController.update)
+router.put('/:id', upload.any(), productsController.update)
 
 // /*** DELETE ONE PRODUCT***/
 router.delete('/delete/:id', productsController.destroy)
